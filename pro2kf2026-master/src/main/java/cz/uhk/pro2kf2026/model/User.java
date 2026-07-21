f39dfeb8cc75431897c24084a57fbde9a88b8661
dominik.simon <simi.dominik1@gmail.com>
2026-07-21 23:10:40 +0200

    Doplněny odkazy do user a items

--- pro2kf2026-master/src/main/java/cz/uhk/pro2kf2026/model/User.java
+++ pro2kf2026-master/src/main/java/cz/uhk/pro2kf2026/model/User.java
@@ -11,6 +11,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
+    @Column(unique = true)
     private String username;
     private String password;
     private String role;
